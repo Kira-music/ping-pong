f4c07a5cf513f97f897fbfa7855a038fc2331bb1
Kira-music <164721563+Kira-music@users.noreply.github.com>
2024-04-27 19:06:11 +0500

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,5 +1,7 @@
 from pygame import*
 
+font.init()
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, w, h):
         super().__init__()
@@ -17,7 +19,7 @@ class Player(GameSprite):
 
         if keys_pressed[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 595:
+        if keys_pressed[K_DOWN] and self.rect.y < 400:
             self.rect.y += self.speed
 
     def update2(self):
@@ -25,8 +27,11 @@ class Player(GameSprite):
 
         if keys_pressed[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < 595:
+        if keys_pressed[K_s] and self.rect.y < 400:
             self.rect.y += self.speed
+
+lost1 = 0
+lost2 = 0
 
 class Enemy(GameSprite):
     def __init__(self, player_image, player_x, player_y, player_speed, w, h):
@@ -50,30 +55,62 @@ class Enemy(GameSprite):
 window = display.set_mode((700, 500))
 display.set_caption('Ping_pong')
 
-background = transform.scale(image.load('images.jpg'), (700, 500))
+background = transform.scale(image.load('background.png'), (700, 500))
 
 clock = time.Clock()
 FPS = 60
 
-racket1 = Player('racket.png', 20, 250, 15, 100, 100)
+font1 = font.SysFont('Arial', 36)
+font2 = font.SysFont('Arial', 25)
 
-racket2 = Player('racket.png', 590, 250, 15, 100, 100)
+racket1 = Player('fork.png', 20, 250, 15, 30, 120)
 
-ball = Enemy('ball.png', 200, 300, 1, 40, 40)
+racket2 = Player('fork.png', 630, 250, 15, 30, 120)
+
+ball = Enemy('donut.png', 200, 300, 5, 60, 50)
 
 game = True
 
+finish = False
 while game:
-    window.blit(background, (0, 0))
+    if not finish:
+        window.blit(background, (0, 0))
 
-    racket1.update()
-    racket1.reset()
+        racket2.update()
+        racket2.reset()
 
-    racket2.update2()
-    racket2.reset()
+        racket1.update2()
+        racket1.reset()
 
-    ball.update()
-    ball.reset()
+        ball.update()
+        ball.reset()
+
+        text_1lose = font1.render('Gamer 1 lose', 1, (231, 84, 128))
+        text_2lose = font1.render('Gamer 2 lose', 1, (231, 84, 128))
+
+        gamer1 = font2.render('Gamer 1:'+ str(lost1), 1, (205, 0, 205))
+        window.blit(gamer1, (20, 30))
+
+        gamer2 = font2.render('Gamer 2:'+ str(lost2), 1, (205, 0, 205))
+        window.blit(gamer2, (560, 30))
+
+        if ball.rect.x < 5:
+            lost2 += 1
+            ball.rect.x = 200
+            ball.rect.y = 300
+
+        if ball.rect.x > 680:
+            lost1 += 1
+            ball.rect.x = 200
+            ball.rect.y = 300
+
+        if lost1 == 3:
+            window.blit(text_2lose, (250, 185))
+            finish = True
+
+        if lost2 ==3:
+            window.blit(text_1lose, (250, 185))
+            finish = True
 
     for e in event.get():
         if e.type == QUIT:
